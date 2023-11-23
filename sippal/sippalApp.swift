f8d332525454c2e652f39f4b01da967fbefb561b
Ihor Savenko <csigorek@gmail.com>
2023-11-23 15:30:54 +0000

    Initial Commit

---
+++ sippal/sippalApp.swift
@@ -0,0 +1,17 @@
+//
+//  sippalApp.swift
+//  sippal
+//
+//  Created by Admin on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct sippalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
